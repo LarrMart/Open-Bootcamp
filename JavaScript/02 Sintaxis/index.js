37ec9894ea03fe8d2400384c84cf789a6ee4e366
Guillermo Larroca Martínez <larrocaguillermo@gmail.com>
2023-01-23 19:04:30 -0300

    02 Sintaxis

---
+++ JavaScript/02 Sintaxis/index.js
@@ -0,0 +1,15 @@
+const list = [
+    "Guillermo Larroca Martínez",
+    36,
+    true,
+    new Date(1986, 8, 22),
+    {
+        titulo: "El poder del ahora",
+        autor: "Eckhart Tolle",
+        fecha: "1997",
+        url: "https://es.wikipedia.org/wiki/El_poder_del_Ahora"
+    }
+
+];
+
+console.log(list);
