839294f8e0ffab132b30ecf77ffdb9b695ebafaf
Guillermo Larroca Martínez <larrocaguillermo@gmail.com>
2023-01-25 16:32:54 -0300

    05 Números('Recordar que los números tienen un comportamiento MUY extraño. Indagar más).

---
+++ JavaScript/05 Números/numbers.js
@@ -0,0 +1,8 @@
+let alt_cm = 177;
+let alt_m = 1.77;
+let peso_kg = 80.9;
+let alt_round_up = Math.ceil(alt_m);
+let peso_round_down = Math.floor(peso_kg);
+let bool = Number.MAX_VALUE + 1 === Number.MAX_VALUE; //Qué extraño esto!!!!
+
+console.log(alt_round_up, peso_round_down, bool);
